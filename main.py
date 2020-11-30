83daa306af3ded091beeced29b0849da808cf556
sueyoshiryosuke <sueyoshi.ryosuke@it-neec.jp>
2020-11-30 01:09:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  name = "なまえ"
+  age = 20
+  return render_template('index.html',
+    name=name,
+    age=age)
+#  return "<img src=\"static/yuzu2.jpg\">"
+
+app.run(host='0.0.0.0')
